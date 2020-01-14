9b2c573221be515cab5e81d5a165b91eb8804a6b
MaxDorfmanUCLA <max.dorfman1@gmail.com>
2020-01-14 11:39:45 -0700

    first commit

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,2 +1,24 @@
+var express = require('express');
+const bodyParser = require('body-parser');
+var path = require('path');
 
+
+
+var app = express();
+app.use(express.static(path.join(__dirname, './client/src/index.html')));
+console.log(__dirname);
+app.use(bodyParser.json());  
+app.use(bodyParser.urlencoded({extended: true}));
+
+
+app.listen(4000, function(){
+    console.log('Listening on port 4000...');
+})
+
+
+app.get('/', function(req,res){
+    var resultObj = hashFunc(req.params.password);
+    res.send(resultObj);
+    //add to DB
+})
 
